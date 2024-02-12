5002c64e0b5390b0c5a3355165ffa3eed9b14ea2
Estifanos Taye <estifanostaye11@gmail.com>
2024-02-12 11:17:36 +0300

    creating a window

--- example 1.py
+++ example 1.py
@@ -7,7 +7,7 @@ running = True
 
 while running:
   for event in pygame.event.get():
-    if event == pygame.QUIT:
+    if event.type == pygame.QUIT:
       running = False
 
   screen.fill("purple")
